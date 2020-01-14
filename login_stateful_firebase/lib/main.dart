5d6b3b8513a5a8f7f1f7292bc8cfa35f81a7484a
irfanbaigse <irfan.blackhawk@gmail.com>
2020-01-14 17:07:02 +0400

    # add stream provider

--- login_stateful_firebase/lib/main.dart
+++ login_stateful_firebase/lib/main.dart
@@ -1,15 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:login_stateful_firebase/src/model/user.dart';
+import 'package:login_stateful_firebase/src/services/auth.dart';
 import 'package:login_stateful_firebase/src/wraaper.dart';
+import 'package:provider/provider.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Firebase Jwt Demo',
-      home: Wrapper(),
+    return StreamProvider<User>.value(
+      value: AuthService().user$,
+      child: MaterialApp(
+        title: 'Flutter Firebase Jwt Demo',
+        home: Wrapper(),
+      ),
     );
   }
 }
